e621e0465a6a9b69f9378d5af2edc99739ce1ee6
inoplanetyanya <stepanov.naruto@yandex.ru>
2021-11-25 09:04:50 +0300

    layout changes; restyles; scss

---
+++ app/static/app/scripts/resize.js
@@ -0,0 +1,5 @@
+$(function() {
+  // let maxWidth = 0;
+  // $('.center-part').map(function() { if ($(this).width() > maxWidth) maxWidth = $(this).width() });
+  // $('.center-part').map(function() {$(this).width(maxWidth + 10)})
+});
